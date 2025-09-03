4e3ba2884754dba42d56b41dc44a45c068ad16c1
noahcoook <cooknoah04@gmail.com>
2025-09-03 12:09:22 -0700

    chapter 3 edits

--- Assignments/CH3_loops_ncook.py
+++ Assignments/CH3_loops_ncook.py
@@ -1,5 +1,15 @@
-fruits = ["apple", "banana", "cherry", "berry", "blueberry", "strawberry"]
-for x in fruits:
-  if x == "banana":
-    break
-  print(x)
+#countdown timer with sys module
+from calendar import c
+import sys
+
+starting_number = int(input("Enter a starting number for the countdown: ")) #
+
+while starting_number > -1:
+    print('Youre on number ', str(starting_number), '.')
+    starting_number = starting_number - 1
+    if starting_number == 0:
+        print('Youve reached the end!')
+        sys.exit()
+
+    
+
