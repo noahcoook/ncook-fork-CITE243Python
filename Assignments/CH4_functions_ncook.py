d193f1ce6c3279fb7eeebc38cfeb5aa6670dfa94
noahcoook <cooknoah04@gmail.com>
2025-09-03 16:34:03 -0700

    updated ch4

--- Assignments/CH4_functions_ncook.py
+++ Assignments/CH4_functions_ncook.py
@@ -1,3 +1,5 @@
+import sys
+
 def rectangle():
     length = int(input('please enter rectangle length: '))
     width = int(input('please enter rectangle width: '))
@@ -8,9 +10,17 @@ def circle():
     radius = int(input('please enter circle radius: '))
     circum = 3.14159 * (radius ** 2)
     area = 2* 3.14159 * radius
-    area = str(round(area, 2))
-    circum = str( round (circum , 2))
+    area = str(round(area, 2)) #rounds down area to 2 places, also converts that value into a str
+    circum = str( round (circum , 2)) #same but for circum
     return print('youre circle area is ', area, ', youre circumference is ', circum, '.')
 
-#rectangle()
-circle()
+user_input = input("would you like to find area of rectangle or circle? ")
+
+if user_input == 'circle' or user_input == 'Circle':
+    circle()
+    sys.exit()
+ 
+if user_input == 'rectangle' or user_input == 'Rectangle':
+    rectangle()
+    sys.exit
+    
