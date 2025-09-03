f44d06c7d41d91307ef41e807a69643ef7bbb55c
noahcoook <cooknoah04@gmail.com>
2025-09-03 13:50:06 -0700

    edited chapter 4, did rectangle and circle area calc

--- Assignments/CH4_functions_ncook.py
+++ Assignments/CH4_functions_ncook.py
@@ -1,1 +1,11 @@
+#def area(length, width):
+#   return length * width
+#size = str(area(5, 20))
+#print(str(area(3,24)))
 
+def circle(radius):
+    area = 3.14159 * (radius ** 2)
+    circumference =  2 * 3.14159 * radius
+    return area
+    return circumference
+print(str(circle(12.3232)))
