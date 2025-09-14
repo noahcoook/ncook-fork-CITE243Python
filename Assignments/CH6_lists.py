b6c437eb2bac7dc809e576e3a648699b991e66b0
noahcoook <cooknoah04@gmail.com>
2025-09-13 21:09:03 -0700

    Changed 'list' in fuction to 'items' since list is already built into python

--- Assignments/CH6_lists.py
+++ Assignments/CH6_lists.py
@@ -1,24 +1,24 @@
-def print_list(list):
-    if len(list) == 0: 
+def print_list(items):
+    if len(items) == 0: 
     #letting user know if there are no entries in the list
         result = 'Please enter atleast one item in list'
         return result
-    if len(list) == 1: #if one item in list, print out just the one entry
-        for name in list:
+    if len(items) == 1: #if one item in list, print out just the one entry
+        for name in items:
             result = name
         return result
-    if len(list) == 2: # Handle two items case - format as "item1 and item2"
+    if len(items) == 2: # Handle two items case - format as "item1 and item2"
         result = ''
-        for names in list:
-            if names == list[-1]:
+        for names in items:
+            if names == items[-1]:
                 result += ' and ' + str(names)
             else:
                 result += str(names)
         return result
-    if len(list) > 2: # Handle 3+ items case - format as "item1, item2 and item3"
+    if len(items) > 2: # Handle 3+ items case - format as "item1, item2 and item3"
         result = ''
-        for names in list:
-            if names == list[-1]:
+        for names in items:
+            if names == items[-1]:
                 result += 'and ' + str(names) 
             else:
                 result += str(names) + ', '
@@ -31,21 +31,15 @@ food_list2 = ['beans', 'rice'] #two entries
 food_list4 = ['beans', 'rice', 'toast', 'bread'] #3+ entries
 food_list5 = ['beans', 'rice', 'toast', 'bread', 7, 1.0] #3+ entries + int + float
 
-#executes functions
-x = print_list(food_list0)
-y = print_list(food_list1)
-z = print_list(food_list2)
-a = print_list(food_list4)
-b = print_list(food_list5)
-
 #prints functions
-print(x)
-print(y)
-print(z)
-print(a)
-print(b)
-print(type(x)) #checked that data type is in fact str
-print(type(y))
-print(type(z))
-print(type(a))
-print(type(b))
+print(print_list(food_list0))
+print(print_list(food_list1))
+print(print_list(food_list2))
+print(print_list(food_list4))
+print(print_list(food_list5))
+
+# print(type(x)) #checked that data type is in fact str
+# print(type(y))
+# print(type(z))
+# print(type(a))
+# print(type(b))
