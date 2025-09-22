bc70a6bdc731f932eab0e26bc6f1ac5b2f0b08aa
noahcoook <cooknoah04@gmail.com>
2025-09-22 09:41:36 -0700

    added ch9 and 10

---
+++ Assignments/CH8_strings_ncook.py
@@ -0,0 +1,8 @@
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+
+for data in range(len(tableData)):
+    print(data)
+
+
