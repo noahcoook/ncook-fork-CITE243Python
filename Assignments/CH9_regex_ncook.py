65d44d14d343e5655a0b048b967adb6f92bc6aa4
noahcoook <cooknoah04@gmail.com>
2025-09-24 10:48:22 -0700

    regex update

--- Assignments/CH9_regex_ncook.py
+++ Assignments/CH9_regex_ncook.py
@@ -1,2 +1,23 @@
 import re
 
+password = input('enter a password >\n')
+
+def passwordVal(passwd):
+    value = False
+    while value is True:
+        if len(passwd) < 8:
+            print('min length is 8 characters')
+            value = 
+            #return False
+        elif not re.search(r'[0-9]+', passwd):
+            print('min one number')
+            #return False
+        elif not re.search(r'[A-Z]+', passwd):
+            print('min one capital letter')
+            #return False   
+        elif not re.search(r'[a-z]+', passwd):
+            print('min one lowercase letter')
+            #return False
+        return True
+
+passwordVal(password)
