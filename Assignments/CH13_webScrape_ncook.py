7f51f3712457b449908b85980881000445b438ca
noahcoook <cooknoah04@gmail.com>
2025-10-19 14:56:20 -0700

    done with ch13

--- Assignments/CH13_webScrape_ncook.py
+++ Assignments/CH13_webScrape_ncook.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
 import random, time
 
 browser = webdriver.Firefox()
@@ -9,7 +11,8 @@ time.sleep(5)
 random_keys = [Keys.UP, Keys.DOWN, Keys.LEFT, Keys.RIGHT]
 sent_keys = ''
 
-for i in range(1, 100):
+for i in range(1, 300):    
     sent_keys = random.choice(random_keys)
-    send_keys(sent_keys)
-    time.sleep(1)
+    html_elem = browser.find_element(By.TAG_NAME, 'html')
+    html_elem.send_keys(sent_keys)
+    time.sleep(.1)
